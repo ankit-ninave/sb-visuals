b7d8e2c370547d15e5664cf7a3d4fe6a57767cc9
MICROPROINDIA\aninave <aninave@microproindia.com>
2026-04-14 14:58:34 +0530

    theme changes icon and premium feel

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'sb-visuals' title`, () => {
+  it(`should have the 'SbVisuals' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sb-visuals');
+    expect(app.title).toEqual('SbVisuals');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sb-visuals');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SbVisuals');
   });
 });
